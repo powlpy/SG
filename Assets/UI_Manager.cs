993ca2aa1e707b8c103e5a75f468828223f63de1
DESKTOP\Theo <theo_j@orange.fr>
2016-12-13 00:03:44 +0100

    New Start Menu

--- Assets/UI_Manager.cs
+++ Assets/UI_Manager.cs
@@ -6,6 +6,7 @@ using UnityEngine.SceneManagement;
 public class UI_Manager : MonoBehaviour {
 
     public GameObject panelOptions;
+    public GameObject cloud1,cloud2;
 
 	// Use this for initialization
 	void Start () {
@@ -14,8 +15,19 @@ public class UI_Manager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+
+        //--- Animation nuages---
+        cloud1.transform.Translate(Vector3.left * 0.008f);
+        cloud2.transform.Translate(Vector3.left * 0.005f);
+        if(cloud1.transform.position.x < -12f)
+        {
+            cloud1.transform.position = new Vector3(12f, cloud1.transform.position.y, cloud2.transform.position.z);
+        }
+        if (cloud2.transform.position.x < -12f)
+        {
+            cloud2.transform.position = new Vector3(12f, cloud2.transform.position.y, cloud2.transform.position.z);
+        }
+    }
 
    public void StartNewGame()
     {
